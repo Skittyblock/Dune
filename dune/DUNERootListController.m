67b059cb729c9dd8c0ba1adb8256f6d1bc5402a2
Skitty <mrskittyblock@gmail.com>
2019-07-26 17:32:56 -0500

    better preferences

--- dune/DUNERootListController.m
+++ dune/DUNERootListController.m
@@ -1,70 +1,48 @@
+// Dune Settings Controller
+
 #include "DUNERootListController.h"
 
 #define kTintColor [UIColor colorWithRed:0.09 green:0.12 blue:0.16 alpha:1.0]
 
-@interface DUNEHeader : UITableViewCell {
-  UILabel *label;
-  UILabel *underLabel;
-}
+@interface DUNEHeader : UITableViewCell
 @end
 
 @implementation DUNEHeader
 - (id)initWithSpecifier:(PSSpecifier *)specifier {
   self = [super initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"Cell"];
+
   if (self) {
-		#define kWidth [[UIApplication sharedApplication] keyWindow].frame.size.width
-		CGRect labelFrame = CGRectMake(0, -15, kWidth, 80);
-		CGRect underLabelFrame = CGRectMake(0, 35, kWidth, 60);
+    UILabel *title = [[UILabel alloc] initWithFrame:CGRectMake(0, 30, self.frame.size.width, 60)];
+    title.numberOfLines = 1;
+    title.font = [UIFont systemFontOfSize:50];
+    title.text = @"Dune";
+    title.textColor = kTintColor;
+    title.textAlignment = NSTextAlignmentCenter;
+    [self addSubview:title];
 
-		label = [[UILabel alloc] initWithFrame:labelFrame];
-		[label setNumberOfLines:1];
-		label.font = [UIFont systemFontOfSize:50];
-		[label setText:@"Dune"];
-		//[label setBackgroundColor:[UIColor clearColor]];
-		label.textColor = kTintColor;
-		label.textAlignment = NSTextAlignmentCenter;
-
-		underLabel = [[UILabel alloc] initWithFrame:underLabelFrame];
-		[underLabel setNumberOfLines:1];
-		underLabel.font = [UIFont systemFontOfSize:20];
-		[underLabel setText:@"A Free iOS Dark Mode"];
-		//[underLabel setBackgroundColor:[UIColor clearColor]];
-		underLabel.textColor = [UIColor grayColor];
-		underLabel.textAlignment = NSTextAlignmentCenter;
-
-		[self addSubview:label];
-		[self addSubview:underLabel];
+    UILabel *subtitle = [[UILabel alloc] initWithFrame:CGRectMake(0, 85, self.frame.size.width, 30)];
+    subtitle.numberOfLines = 1;
+    subtitle.font = [UIFont systemFontOfSize:20];
+    subtitle.text = @"A Free iOS Dark Mode";
+    subtitle.textColor = [UIColor grayColor];
+    subtitle.textAlignment = NSTextAlignmentCenter;
+    [self addSubview:subtitle];
   }
+
   return self;
 }
+
 - (CGFloat)preferredHeightForWidth:(CGFloat)arg1 {
-  CGFloat prefHeight = 75.0;
-  return prefHeight;
+  return -150.0;
 }
 @end
 
 @implementation DUNERootListController
-- (id)init {
-	self = [super init];
-	if (self) {
-    UIBarButtonItem *respringButton = [[UIBarButtonItem alloc] initWithTitle:@"Respring" style:UIBarButtonItemStylePlain target:self action:@selector(respring)];
-    self.navigationItem.rightBarButtonItem = respringButton;
-	}
-	return self;
-}
-
 - (NSArray *)specifiers {
-	if (!_specifiers) {
-		_specifiers = [[self loadSpecifiersFromPlistName:@"Root" target:self] retain];
-	}
+  if (!_specifiers) {
+    _specifiers = [self loadSpecifiersFromPlistName:@"Root" target:self];
+  }
 
-	return _specifiers;
-}
-
-- (void)respring {
-  NSTask *task = [[[NSTask alloc] init] autorelease];
-  [task setLaunchPath:@"/usr/bin/killall"];
-  [task setArguments:[NSArray arrayWithObjects:@"backboardd", nil]];
-  [task launch];
+  return _specifiers;
 }
 @end
